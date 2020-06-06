acef111d6f3ca096df0118fc845f4439278a6a80
HenryHe613 <2193276171@qq.com>
2020-06-06 11:05:44 +0800

    Site updated: 2020-06-06 11:05:42

---
+++ js/click_show_text.js
@@ -0,0 +1,38 @@
+var a_idx = 0;
+jQuery(document).ready(function ($) {
+    $("body").click(function (e) {
+        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+            y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#FF0000"
+        });
+        $("body").append($i);
+        $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            3000,
+            function () {
+                $i.remove();
+            });
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
+
+
+作者: Yafine
+链接: https://yafine-blog.cn/posts/8c84.html#鼠标点击文字特效
+来源: 过客~励む
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
